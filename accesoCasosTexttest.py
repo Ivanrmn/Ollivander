57b40809fbd74692d5da2d9bd2b51ae995ef39d3
ivanrmn <ivanrmnh@gmail.com>
2017-12-05 07:32:28 +0100

    correcciones y docstring accesoCasosTexttest

--- accesoCasosTexttest.py
+++ accesoCasosTexttest.py
@@ -1,4 +1,6 @@
 def accesoCasosTexttest(matrizCasosTest, rutaAccesoFichero):
+    """rutaAccesoFichero abre un fichero en modo lectura si lo encuentra, sino mostrará un mensaje.
+    matrizCasosTest crea una lista vacía y al llegar al return, debería mostrar los datos del fichero casos_test"""
 
      try:
         if not isinstance(rutaAccesoFichero, str):
@@ -23,7 +25,7 @@ def accesoCasosTexttest(matrizCasosTest, rutaAccesoFichero):
             else:
                 int matrizCasosTest[1]
                 int matrizCasosTest[2]
-                #item = linea.rstrip().rsplit(',', maxsplit=numeroPropiedadesItem - 1)
+                item = linea.rstrip().rsplit(',', maxsplit=numeroPropiedadesItem - 1)
                 casosTestDia.append(item)
         fichero.close()
         return matrizCasosTest
