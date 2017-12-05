0914e27640c39e353974df01ebfcb3e68a7df372
ivanrmn <ivanrmnh@gmail.com>
2017-12-05 07:47:25 +0100

    preguntas contestadas

--- casosTestOllivander.py
+++ casosTestOllivander.py
@@ -1,3 +1,10 @@
+"""2. Concluye: ¿qué problemas presenta el código?
+	Trataba las números de la lista como string, así que no podía calcular lo que necesitaba."""
+
+"""4. Que es la deuda técnica.
+	Es el coste a pagar por hacer rápido y mal las cosas. A medida que avanza el tiempo, la deuda aumenta, 
+	porque cada vez que se trabaja más sobre un código mal hecho, más se deberá corregir hasta solucionar el problema."""
+
 """Importamos las funciones al archivo principal"""
 
 from accesoCasosTexttest import accesoCasosTexttest
